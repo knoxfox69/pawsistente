5b32af38196cf248c5a290b204fa7bd45d5879e6
Juan Pablo Hernandez Corcuera <juanjphc@gmail.com>
2025-10-17 15:19:07 -0600

    chore: update launch date in configuration for countdown timer to reflect new release schedule

--- src/lib/config/launchDate.ts
+++ src/lib/config/launchDate.ts
@@ -4,5 +4,5 @@
 // FOR TESTING: Simply change this date to a past date to bypass the countdown
 // Example: '2024-01-01T00:00:00.000Z' (already passed)
 // 
-// Current setting: October 17, 2025 at 3:12 PM GMT-6
-export const LAUNCH_DATE = '2025-10-17T21:15:20.000Z'; // 3:12pm GMT-6 = 9:12pm UTC
+// Current setting: October 20, 2025 at 3:00 PM GMT-6
+export const LAUNCH_DATE = '2025-10-20T21:00:00.000Z'; // 3:00pm GMT-6 = 9:00pm UTC
